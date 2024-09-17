c846f969508580c11d0b1076fb5e702a75b74f69
RiFors <rodnokozov01@gmail.com>
2024-09-18 00:13:10 +0300

    Homework_No_7

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -8,15 +8,27 @@ using Utilities;
 namespace UnitBrains.Player
 {
     public class SecondUnitBrain : DefaultPlayerUnitBrain
-    { 
+    {
+        public SecondUnitBrain()
+        {
+            Id = IdBilling++;
+        }
+
+        public static int IdBilling = 0;
+        public int Id;
+
         public override string TargetUnitName => "Cobra Commando";
+
         private const float OverheatTemperature = 3f;
         private const float OverheatCooldown = 2f;
+        private const int MaxTarget = 3;
+
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
 
         private Vector2Int _notRangeEnemyPosition;
+        private List<Vector2Int> allTargetEnemies = new List<Vector2Int>();
 
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
@@ -47,69 +59,54 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            ///
-            Vector2Int importantTargetEnemies = new Vector2Int();
 
-            List<Vector2Int> allTargetEnemies = GetAllTargets().ToList();
+            allTargetEnemies.Clear();
+            allTargetEnemies =  GetAllTargets().ToList();
 
             Vector2Int targetBaseEnemy = runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId];
 
-            //allTargetEnemies.Remove(targetBaseEnemy);
+            allTargetEnemies.Remove(targetBaseEnemy);
 
             if (allTargetEnemies.Count > 0)
             {
+                SortByDistanceToOwnBase(allTargetEnemies);
+
+                if (allTargetEnemies.Count > MaxTarget)
+                {
+                    allTargetEnemies = allTargetEnemies.GetRange(0, MaxTarget);
+                }
+
+                int idEnemy = (Id - 1) % allTargetEnemies.Count;
+
+                Vector2Int target = allTargetEnemies[idEnemy];
+
+                allTargetEnemies.Clear();
+
                 if (!HasTargetsInRange())
                 {
-                    _notRangeEnemyPosition = GetDangerousEnemy(allTargetEnemies);
+                    _notRangeEnemyPosition = target;
                 }
                 else
                 {
-                    importantTargetEnemies = GetDangerousEnemy(allTargetEnemies);
+                    allTargetEnemies.Add(target);
                 }
             }
             else
             {
+                allTargetEnemies.Clear();
 
                 if (!HasTargetsInRange())
                 {
                     _notRangeEnemyPosition = targetBaseEnemy;
-
                 }
                 else
                 {
-                    importantTargetEnemies = targetBaseEnemy;
-                }
-            }
 
-            if (!(importantTargetEnemies == Vector2Int.zero))
-            {
-                allTargetEnemies.Clear();
-                allTargetEnemies.Add(importantTargetEnemies);
-            }
-            else
-            {
-                allTargetEnemies.Clear();
+                    allTargetEnemies.Add(targetBaseEnemy);
+                }
             }
 
             return allTargetEnemies;
-            ///////////////////////////////////////
-        }
-
-        private Vector2Int GetDangerousEnemy(List<Vector2Int> targetEnemies)
-        {
-            Vector2Int minDistantEnemy = new(int.MaxValue, int.MaxValue);
-
-            foreach (Vector2Int enemy in targetEnemies)
-            {
-                if (DistanceToOwnBase(minDistantEnemy) < DistanceToOwnBase(enemy))
-                {
-                    continue;
-                }
-
-                minDistantEnemy = enemy;
-            }
-
-            return minDistantEnemy;
         }
 
         public override void Update(float deltaTime, float time)
